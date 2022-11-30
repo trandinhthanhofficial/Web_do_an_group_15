e661e4f655418d90b2666ace3046676dd81e9115
Đình Thành <nh Thnh@ASUS-TUF-GAMING>
2022-11-30 09:32:13 +0700

    Add project files.

---
+++ Web_do_an/Controllers/ClassesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_do_an.Controllers
+{
+    public class ClassesController : Controller
+    {
+        // GET: Classes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web_do_an/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web_do_an.MvcApplication" Language="C#" %>
